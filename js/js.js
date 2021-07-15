e5a2efc20322729f002752630040c90a9706410d
朱利云 <fd@qq.com>
2021-07-15 18:00:39 +0800

    增加历史订单

--- js/js.js
+++ js/js.js
@@ -4,6 +4,7 @@
   var faultByHourIndex2 = 0; //播放所在下标
   var faultByHourIndex4 = 0; //播放所在下标
   var faultByHourIndex5 = 0; //播放所在下标
+  var faultByHourIndex6 = 0; //播放所在下标
   largeScreen();
   setInterval(function () {
     largeScreen();
@@ -11,7 +12,8 @@
   function largeScreen() {
     $.ajax({
       url: "https://api.zuche.miway.com/largeScreen",
-//       url: "http://47.98.134.89:20008/largeScreen",
+      // url: "http://192.168.1.119:20008/largeScreen",
+      // url: "http://ymzc-api.fintechzh.com/largeScreen",
       type: "POST",
       context: document.body,
       dataType: "JSON",
@@ -25,7 +27,7 @@
         echarts_31(data.ageDistributionVOS);
         echarts_33(data.cityVehiclesCircle);
         echarts_5(data.areaDistributionVO);
-        echarts_6(data.agentOrderProportionVO);
+        echarts_6(data.orderByMonthVO);
         $("#countOrder").html(data.countOrder);
         $("#countUsers").html(data.countUsers);
         $("#countCertifyUsers").html(data.countCertifyUsers);
@@ -54,12 +56,18 @@
         } else {
           faultByHourIndex5++;
         }
+        if (faultByHourIndex6 > data.orderByMonthVO.countOrder.length) {
+          faultByHourIndex6 = 0;
+        } else {
+          faultByHourIndex6++;
+        }
         console.log(
           faultByHourIndex,
           faultByHourIndex1,
           faultByHourIndex2,
           faultByHourIndex4,
-          faultByHourIndex5
+          faultByHourIndex5,
+          faultByHourIndex6
         );
       },
     });
@@ -253,7 +261,7 @@
           type: "bar",
           data: data.data,
           barWidth: "35%", //柱子宽度
-          // barGap: 1, //柱子之间间距
+          barGap: 1, //柱子之间间距
           itemStyle: {
             normal: {
               color: "#2f89cf",
@@ -664,85 +672,220 @@
 
   function echarts_6(data) {
     // 基于准备好的dom，初始化echarts实例
+
+    // 基于准备好的dom，初始化echarts实例
+    console.log(data);
     var myChart = echarts.init(document.getElementById("echart6"));
-
-    var dataStyle = {
-      normal: {
-        label: {
-          show: false,
-        },
-        labelLine: {
-          show: false,
-        },
-        shadowBlur: 40,
-        shadowColor: "rgba(40, 40, 40, 1)",
-      },
-    };
-    var placeHolderStyle = {
-      normal: {
-        color: "rgba(255,255,255,.05)",
-        label: { show: false },
-        labelLine: { show: false },
-      },
-      emphasis: {
-        color: "rgba(0,0,0,0)",
-      },
-    };
-    var convertData = function (data) {
-      var res = [];
-      var city = data.city;
-      var data = data.data;
-      for (var i = 0; i < data.length; i++) {
-        res.push({
-          name: city[i],
-          type: "pie",
-          clockWise: false,
-          center: ["50%", "40%"],
-          radius: [`${59 - i * 10}%`, `${70 - i * 10}%`],
-          itemStyle: dataStyle,
-          hoverAnimation: false,
-          data: [
-            {
-              value: data[i],
-              name: i,
-            },
-            {
-              value: 100 - data[i],
-              name: "invisible",
-              tooltip: { show: false },
-              itemStyle: placeHolderStyle,
-            },
-          ],
-        });
-      }
-      console.log(res);
-      return res;
-    };
     option = {
-      color: ["#0f63d6", "#0f78d6", "#0f8cd6", "#0fa0d6", "#0fb4d6"],
       tooltip: {
-        show: true,
-        formatter: "{a} : {c} ",
-      },
-      legend: {
-        itemWidth: 10,
-        itemHeight: 10,
-        itemGap: 12,
-        bottom: "3%",
-        data: data.city,
-        textStyle: {
-          color: "rgba(255,255,255,.6)",
+        trigger: "axis",
+        axisPointer: {
+          lineStyle: {
+            color: "#dddc6b",
+          },
         },
       },
-      // series: []
-      series: convertData(data),
-    };
+      //  legend: {
+      //    top: "0%",
+      //    data: ["新增"],
+      //    textStyle: {
+      //      color: "rgba(255,255,255,.5)",
+      //      fontSize: "12",
+      //    },
+      //  },
+      grid: {
+        left: "10",
+        top: "30",
+        right: "10",
+        bottom: "10",
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: false,
+          axisLabel: {
+            textStyle: {
+              color: "rgba(255,255,255,.6)",
+              fontSize: 12,
+            },
+            interval: 3,
+            //  rotate:-20
+          },
+          axisLine: {
+            lineStyle: {
+              color: "rgba(255,255,255,.2)",
+            },
+          },
+          data: data.months,
+        },
+        {
+          axisPointer: { show: false },
+          axisLine: { show: false },
+          position: "bottom",
+          offset: 20,
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+          axisTick: { show: false },
+          axisLine: {
+            lineStyle: {
+              color: "rgba(255,255,255,.1)",
+            },
+          },
+          axisLabel: {
+            textStyle: {
+              color: "rgba(255,255,255,.6)",
+              fontSize: 12,
+            },
+          },
 
+          splitLine: {
+            lineStyle: {
+              color: "rgba(255,255,255,.1)",
+            },
+          },
+        },
+      ],
+      series: [
+        {
+          name: "订单数",
+          type: "line",
+          smooth: true,
+          symbol: "circle",
+          symbolSize: 5,
+          showSymbol: false,
+          lineStyle: {
+            normal: {
+              color: "#0184d5",
+              width: 2,
+            },
+          },
+          areaStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(
+                0,
+                0,
+                0,
+                1,
+                [
+                  {
+                    offset: 0,
+                    color: "rgba(1, 132, 213, 0.4)",
+                  },
+                  {
+                    offset: 0.8,
+                    color: "rgba(1, 132, 213, 0.1)",
+                  },
+                ],
+                false
+              ),
+              shadowColor: "rgba(0, 0, 0, 0.1)",
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: "#0184d5",
+              borderColor: "rgba(221, 220, 107, .1)",
+              borderWidth: 12,
+            },
+          },
+          data: data.countOrder,
+        },
+      ],
+    };
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
     window.addEventListener("resize", function () {
       myChart.resize();
     });
+    // 动态显示tootip
+    myChart.dispatchAction({
+      type: "showTip", // 根据 tooltip 的配置项显示提示框。
+      seriesIndex: 0,
+      dataIndex: faultByHourIndex6,
+    });
+
+    // var myChart = echarts.init(document.getElementById("echart6"));
+    // var dataStyle = {
+    //   normal: {
+    //     label: {
+    //       show: false,
+    //     },
+    //     labelLine: {
+    //       show: false,
+    //     },
+    //     shadowBlur: 40,
+    //     shadowColor: "rgba(40, 40, 40, 1)",
+    //   },
+    // };
+    // var placeHolderStyle = {
+    //   normal: {
+    //     color: "rgba(255,255,255,.05)",
+    //     label: { show: false },
+    //     labelLine: { show: false },
+    //   },
+    //   emphasis: {
+    //     color: "rgba(0,0,0,0)",
+    //   },
+    // };
+    // var convertData = function (data) {
+    //   var res = [];
+    //   var city = data.city;
+    //   var data = data.data;
+    //   for (var i = 0; i < data.length; i++) {
+    //     res.push({
+    //       name: city[i],
+    //       type: "pie",
+    //       clockWise: false,
+    //       center: ["50%", "40%"],
+    //       radius: [`${59 - i * 10}%`, `${70 - i * 10}%`],
+    //       itemStyle: dataStyle,
+    //       hoverAnimation: false,
+    //       data: [
+    //         {
+    //           value: data[i],
+    //           name: i,
+    //         },
+    //         {
+    //           value: 100 - data[i],
+    //           name: "invisible",
+    //           tooltip: { show: false },
+    //           itemStyle: placeHolderStyle,
+    //         },
+    //       ],
+    //     });
+    //   }
+    //   console.log(res);
+    //   return res;
+    // };
+    // option = {
+    //   color: ["#0f63d6", "#0f78d6", "#0f8cd6", "#0fa0d6", "#0fb4d6"],
+    //   tooltip: {
+    //     show: true,
+    //     formatter: "{a} : {c} ",
+    //   },
+    //   legend: {
+    //     itemWidth: 10,
+    //     itemHeight: 10,
+    //     itemGap: 12,
+    //     bottom: "3%",
+    //     data: data.city,
+    //     textStyle: {
+    //       color: "rgba(255,255,255,.6)",
+    //     },
+    //   },
+    //   // series: []
+    //   series: convertData(data),
+    // };
+
+    // // 使用刚指定的配置项和数据显示图表。
+    // myChart.setOption(option);
+    // window.addEventListener("resize", function () {
+    //   myChart.resize();
+    // });
   }
 
   function echarts_31(data) {
@@ -751,7 +894,7 @@
     option = {
       title: [
         {
-          text: "年龄分布",
+          text: "用户年龄分布",
           left: "center",
           textStyle: {
             color: "#fff",
@@ -823,7 +966,7 @@
     option = {
       title: [
         {
-          text: "地区分布",
+          text: "用户户籍分布",
           left: "center",
           textStyle: {
             color: "#fff",
